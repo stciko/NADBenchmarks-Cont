4622818dc81972d55498adf847269dfafac85045
KiraCodingBot <helloworld.stephanie@gmail.com>
2022-11-25 01:10:01 -0500

    fix login&logout

--- server/application/forms.py
+++ server/application/forms.py
@@ -21,6 +21,24 @@ class LoginForm(FlaskForm):
     def get_user(self):
         return User.objects(username=self.username).first()
 
+
+class SubmitForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    description = StringField('description', validators=[DataRequired()])
+    image_url = StringField('image_url', validators=[DataRequired()])
+    data_source = StringField('data_source', validators=[DataRequired()])
+    size = StringField('size', validators=[DataRequired()])
+    timespan = StringField('timespan', validators=[DataRequired()])
+    geo_coverage = StringField('geo_coverage', validators=[DataRequired()])
+    published = StringField('published', validators=[DataRequired()])
+    task_type = StringField('task_type', validators=[DataRequired()])
+    topics = StringField('topics', validators=[DataRequired()])
+    data_type = StringField('data_type', validators=[DataRequired()])
+    paper_url = StringField('paper_url', validators=[DataRequired()])
+    reference = StringField('reference', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
         
 
     
