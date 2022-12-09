fb8c0d302f7fdf0b5659afe6a525cb0899548cfc
Danni Shi <82932047+DanniBot@users.noreply.github.com>
2022-12-09 16:17:46 -0500

    Update __init__.py

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -29,7 +29,7 @@ except ImportError:
 
 api=Api()
 
-app=Flask(__name__, static_folder='client/build', static_url_path='')
+app=Flask(__name__, static_folder='./client/build', static_url_path='/')
 app.config.from_object(Config)
 
 db=MongoEngine()
