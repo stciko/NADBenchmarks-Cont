a1bfa502ce96d21b3cb1b5108f1fa1280d68ed32
Bao Tran <quoc.bao.tran1218@gmail.com>
2022-12-09 16:22:29 -0500

    change static url

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -28,7 +28,7 @@ except ImportError:
 
 
 api=Api()
-app=Flask(__name__, static_folder='../../client/build')
+app=Flask(__name__, static_folder='../../client/build', static_url_path='/')
 app.config.from_object(Config)
 
 db=MongoEngine()
