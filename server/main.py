6a3bb32580508017c076560419377fdafb07626f
stciko <93011238+stciko@users.noreply.github.com>
2023-04-20 14:46:01 -0400

    revert exposure

--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 from application import app
 import os
 
-# if __name__ == "__main__":
-app.run(host='0.0.0.0', debug=False, port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=False, port=int(os.environ.get("PORT", 5000)))
